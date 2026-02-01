a4aa0f0e138096172ca587fd6e9bc0e78136076e
tajul islam <tajulislam199595@gmail.com>
2026-02-01 23:31:36 +0600

    secod-commit

--- src/modules/category/cotegory.router.ts
+++ src/modules/category/cotegory.router.ts
@@ -1,9 +1,10 @@
 import express from 'express'
 import { categoryController } from './catrgory.controller'
+import auth, { UserRole } from '../../middleware/auth'
 const router=express.Router()
-router.get('/',categoryController.getAllCategory)
-router.get('/:name',categoryController.findCategoryByName)
-router.post('/',categoryController.createCategory)
+router.get('/',auth(UserRole.ADMIN,UserRole.CUSTOMER,UserRole.PROVIDER)  ,categoryController.getAllCategory)
+router.get('/:name',auth(UserRole.ADMIN,UserRole.CUSTOMER,UserRole.PROVIDER),categoryController.findCategoryByName)
+router.post('/',auth(UserRole.ADMIN,UserRole.CUSTOMER,UserRole.PROVIDER),categoryController.createCategory)
 
 
 export const categoryRouter=router
